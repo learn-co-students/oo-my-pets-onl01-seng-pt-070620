bd69c597b8314df3058f45aac8d4d129292dc946
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-31 12:03:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
-  # code goes here
+ attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def intialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+    owner.dogs << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+   def owner=(owner)
+     @owner = Owner.name  
+     owner.buy_cat(self) unless owner.cats.include?(self)
+   end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @cats = []
+    @dogs = []
+  end
+
+  def species
+    @species  
+  end
+  
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count  
+  end
+  
+  def self.reset_all
+    self.all.clear  
+  end
+  
+  def cats
+    @cats  
+  end
+
+  def buy_cat(cat)
+      @cats << cats
+      cat.owner = self unless cat.owner == self
+  end
+
 end
