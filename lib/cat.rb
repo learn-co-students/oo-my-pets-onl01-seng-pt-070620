5c0760528170ac5b73c33e90fb87e5b1e81d9c6e
Keesha Hargrove <kharg14@gmail.com>
2020-08-01 01:23:42 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
 class Cat
   # code goes here
+  
+ attr_accessor :mood
+  @@all = []
+  # code goes here
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    owner.cats << self
+    @@all << self
+  end
+
+def name
+  @name
+end
+
+def self.all
+  @@all
+end
+  
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
   # code goes here
+  attr_accessor :mood
+  @@all = []
+  # code goes here
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    owner.dogs<< self
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+      @@all
+  end
 end
