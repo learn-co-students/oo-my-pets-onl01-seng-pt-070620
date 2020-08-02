3d657c9077b20f726e0f0463ae5a60a4409e9d73
Knika Dass <knikadass@gmail.com>
2020-08-02 19:53:33 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,80 @@
+require "pry"
 class Owner
-  # code goes here
+  #attr_reader :species
+  attr_accessor :name, :pets #:dog, :cat
+
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+
+  def initialize(name)
+    #@species = species
+    @name = name
+    @@all << self
+  end
+   
+  def name
+    @name
+  
+  end
+  
+  def species 
+    @species = "human"
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+   # self.species
+   # binding.pry
+    "I am a #{self.species}."
+  end
+
+  def pets
+    @@pets
+  end
+
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+
+  def walk_dogs
+    @@pets[:dogs].each do |dog|
+      dog.mood = 'happy'
+    end
+  end
+
+  def play_with_cats
+    @@pets[:cats].each do |cat|
+      cat.mood = 'happy'
+    end
+  end
+
+  
+  def list_pets
+    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
+  def sell_pets
+    pets.each do |pet, arr|
+      arr.map do |pet|
+        pet.mood = 'nervous'
+      end
+      arr.clear
+    end
+  end
+
 end
