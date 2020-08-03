e9284512e79ffb67d19d0d2501a2d03140af4e29
Katie Le <lethikatie@gmail.com>
2020-08-03 20:12:03 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
-end
+  @@all = []
+attr_accessor :owner, :mood
+attr_reader :name
+ 
+ def initialize(name, owner)
+   @name = name 
+   @mood = "nervous"
+   @owner = owner
+   @@all << self 
+ end 
+ 
+ def self.all
+   @@all
+ end
+ end 
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
-class Owner
-  # code goes here
-end
+class Owner 
+  @@all = []
+  attr_reader :species, :name
+  def initialize(name )
+    @name = name 
+    @species = "human"
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.count
+    @@all.size
+  end 
+  
+  def self.reset_all
+    @@all.clear
+  end 
+  
+  def say_species
+    "I am a #{species}."
+  end 
+end 
