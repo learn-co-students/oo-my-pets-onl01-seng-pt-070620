6331a965916ec883686955dde44c1685ed1663b5
Antonio James <asjames18@gmail.com>
2020-08-04 18:35:07 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Cat
-  # code goes here
+
+  @@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+  def initialize (name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+
+  end
+
+  def self.name
+    @name
+  end
+
+  def self.all
+    @@all
+
+    # binding.pry
+  end
+
+
+
 end
