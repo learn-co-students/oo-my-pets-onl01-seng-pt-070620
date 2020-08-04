6ffd45bfd4273cece892348bea30bb12e60edac1
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 17:26:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(new_name, new_owner)
+    @name = new_name
+    @owner = new_owner
+    @owner.dogs << self
+    @mood = "nervous"
+    @@all << self 
+  end
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.length 
+  end
+  
+  def self.reset_all 
+    @@all = []
+  end
+  
 end
