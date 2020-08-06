83bb7156af49a0d4ae5bc10a836ce8e73cb0f16b
Jill Short <jillian.b.short@gmail.com>
2020-08-06 01:40:57 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,7 +37,48 @@ class Owner
   end
   
   def cats 
-    
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+  
+  def dogs 
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
+    end
+    self.dogs.each do |dog|
+      self.dogs.delete(dog)
+      dog.owner = nil
+      dog.mood = "nervous"
+    end
+  end
+  
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 
 end
