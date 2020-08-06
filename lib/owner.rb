676d1d3c01450b38a89b79dba1f62adccaa5f86f
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-06 18:40:04 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
+require 'pry'
 class Owner
-  # code goes here
+
+attr_reader  :name, :species
+@@all = []
+  
+  def initialize(name)
+    #binding.pry
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+   
+  def say_species
+  "I am a human."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+  self.all.count
+  end
+  
+  def self.reset_all
+  #binding.pry  
+  end
+
+
+
+
+
 end
