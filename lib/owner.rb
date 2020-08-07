a62d4a15efa7b36331bc7bcf3044a44a9de70e84
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-07 02:00:04 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class Owner
 
+
 attr_reader  :name, :species
 @@all = []
 
@@ -36,11 +37,37 @@ attr_reader  :name, :species
   end
   
   def buy_cat(cat)
-   Cat.new(cat,self) 
+    Cat.new(cat,self) 
   end
   
   def buy_dog(dog)
     Dog.new(dog,self)
   end
+
+  def walk_dogs
+    dogs.each {|m| m.mood = "happy"}
+  end
+  
+  def feed_cats
+    cats.each {|m| m.mood = "happy"}
+  end
+  
+  def sell_pets
+    dogs.each do|m| 
+      m.mood = "nervous"
+      m.owner = nil
+    end
+   
+    cats.each do|m| 
+      m.mood = "nervous"
+      m.owner = nil
+   end
+  end
+  
+  def list_pets
+   "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
+  
   
 end
+  
