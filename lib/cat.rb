940ae9fbc470fbd6c5f6a254813bfaf483a64644
Ebo Lee <yiboli.coding@gmail.com>
2020-08-08 16:27:11 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,7 @@
 class Cat
-  # code goes here
+  attr_accessor
+  attr_writer
+  attr_reader
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,39 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def name
+    @name
+  end
+
+  def species
+    @species = "human"
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
 end
